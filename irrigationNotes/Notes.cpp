2dd680d99538445e370e8a81853745f16b58fb83
ianthornton2330 <ianthornton2330@gmail.com>
2016-07-04 21:46:27 -0700

    IrrigationNotes

--- irrigationNotes/Notes.cpp
+++ irrigationNotes/Notes.cpp
@@ -12,3 +12,10 @@ Start Watering Every 24 Hours {
  2: Do X For Y Mins */
 
 /* Line 1 would be Type 1, Lines 2-7 would be Type 2 */
+
+/* Eventual goals:
+    GPRS connectivity - via Arduino GPRS Shield or using an Electron (preferably, because of the IDE/platform/and the pre-existing app)
+    Integrated Hygrometers to measure watering (prevention over/underwatering)
+    Water Metering - and logging how much water is being used per valve (not sure if there's an arduino compatible meter)
+    IFTTT notifications of overages on water usages/anything outside suitable hygrometer ranges/etc.
+*/
