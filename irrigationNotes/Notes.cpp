f29121e71a5a4317e9f7e873497e8bacab07a623
ianthornton2330 <ianthornton2330@gmail.com>
2016-07-04 21:35:02 -0700

    IrrigationNotes
    This is what I remember from making notes last night. I’ll add to it
    what I eventually want to grow it to.

---
+++ irrigationNotes/Notes.cpp
@@ -0,0 +1,14 @@
+Start Watering Every 24 Hours {
+    Turn Pump On and Open Valve 1 for 1 Hour &log ()
+        Close Valve 1 and Open Valve 2 for 1 Hour &log ()
+            Close Valve 2 and Open Valve 3 for 1 Hour &log ()
+                Close Valve 3 and Open Valve 4 for 10 Minutes &log ()
+                    Close Valve 4 and Turn Off Pump &log ()
+                    Wait 23 Hours 50 Minutes &log ()
+                    }
+
+/* 2 types of statements:
+ 1: Do X Every Y Mins
+ 2: Do X For Y Mins */
+
+/* Line 1 would be Type 1, Lines 2-7 would be Type 2 */
