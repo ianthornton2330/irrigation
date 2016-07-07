517dd1b9bfcc1b9b6be04fe40a347187949c715d
ianthornton2330 <ianthornton2330@gmail.com>
2016-07-07 00:00:44 -0700

    TemporaryTimer

---
+++ Examples and Tests/temporary_timer/temporary_timer.ino
@@ -0,0 +1,55 @@
+#define RELAY1  2                        
+#define RELAY2  3                        
+#define RELAY3  4                        
+#define RELAY4  5
+#define RELAY0  6
+
+void setup()
+{
+// Initialise the Arduino data pins for OUTPUT
+  pinMode(RELAY1, OUTPUT);       
+  pinMode(RELAY2, OUTPUT);
+  pinMode(RELAY3, OUTPUT);
+  pinMode(RELAY4, OUTPUT);
+  pinMode(RELAY0, OUTPUT);
+}
+ 
+ void loop()
+{
+/*This step initializes the relay pins. However, they start in the ON position, so we are using just 1 millisecond times to
+initialize the pin into the off state */
+
+   digitalWrite(RELAY1,LOW);           // Turns ON Relays 1
+   delay(1);                                      // Wait 1 milliseconds
+   digitalWrite(RELAY1,HIGH);          // Turns Relay Off
+ 
+   digitalWrite(RELAY2,LOW);           // Turns ON Relays 2
+   delay(1);                                      // Wait 1 milliseconds
+   digitalWrite(RELAY2,HIGH);          // Turns Relay Off
+ 
+   digitalWrite(RELAY3,LOW);           // Turns ON Relays 3
+   delay(1);                                      // Wait 1 milliseconds
+   digitalWrite(RELAY3,HIGH);          // Turns Relay Off
+ 
+   digitalWrite(RELAY4,LOW);           // Turns ON Relays 4
+   delay(1);                                      // Wait 1 milliseconds
+   digitalWrite(RELAY4,HIGH);          // Turns Relay Off
+
+/* Here we are turing the relays on for their allotted times */
+
+    digitalWrite(RELAY1,LOW);           // Turns ON Relays 1
+   delay(3600000);                                      // Wait 1 hour
+   digitalWrite(RELAY1,HIGH);          // Turns Relay Off
+ 
+   digitalWrite(RELAY2,LOW);           // Turns ON Relays 2
+   delay(3600000);                                      // Wait 1 hour
+   digitalWrite(RELAY2,HIGH);          // Turns Relay Off
+ 
+   digitalWrite(RELAY3,LOW);           // Turns ON Relays 3
+   delay(3600000);                                      // Wait 1 hour
+   digitalWrite(RELAY3,HIGH);          // Turns Relay Off
+ 
+   digitalWrite(RELAY4,LOW);           // Turns ON Relays 4
+   delay(600000);                                      // Wait 10 Minutes
+   digitalWrite(RELAY4,HIGH);          // Turns Relay Off     
+ }
