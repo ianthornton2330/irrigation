e049597b41e97a31148e9a53609e64d1c3f97bf5
ianthornton2330 <ianthornton2330@gmail.com>
2016-07-20 00:23:22 -0700

    irrigation update

--- irrigation/irrigation.ino
+++ irrigation/irrigation.ino
@@ -21,62 +21,65 @@ double soil2=0;
 
 
 void setup() {
-  Serial.begin(9600);
-  Particle.variable("soil", soil);
+    Serial.begin(9600);
+    Particle.variable("soil", soil);
 }
+
+int loopCounter = 0;
 
 void loop() {
     
-/*Begin Hygrometer */
+    loopCounter++;
+    /*Begin Hygrometer */
 
-  // read the input on analog pin 0:
-  float sensorValue = analogRead(A0);
-    sensorValue = constrain(sensorValue, 1192, 4095);
+    // read the input on analog pin 0:
+    float sensorValue = analogRead(A0);
+        sensorValue = constrain(sensorValue, 1192, 4095);
     //sensorValue = 2642;
-  float sensorValue2 = analogRead(A1);
-   sensorValue2 = constrain(sensorValue2, 1192, 4095);
+    float sensorValue2 = analogRead(A1);
+        sensorValue2 = constrain(sensorValue2, 1192, 4095);
     //sensorValue2 = 2642;
-  // print out the value you read:
-  //Serial.println(sensorValue);
+    // print out the value you read:
+    //Serial.println(sensorValue);
 
- //map the value to a percentage
-  // Value read minus 1192 (low value), then divided into 2093 (range between 1192 and 4095) = percentage dry (p). Then 100 - (p) will be percent wet.
+    //map the value to a percentage
+    // Value read minus 1192 (low value), then divided into 2093 (range between 1192 and 4095) = percentage dry (p). Then 100 - (p) will be percent wet.
     soil = 100-(((sensorValue-1192)/2903)*100);
     soil2 = 100-(((sensorValue2-1192)/2903)*100);
 
     float result = (soil + soil2) / 2;
 
-/*Begin Anemometer */
+    /*Begin Anemometer */
 
-  sensorValue = analogRead(sensorPin); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
+    sensorValue = analogRead(sensorPin); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
 
-  sensorVoltage = sensorValue * voltageConversionConstant; //Convert sensor value to actual voltage
+    sensorVoltage = sensorValue * voltageConversionConstant; //Convert sensor value to actual voltage
 
-  //Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
+    //Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
     if (sensorVoltage <= voltageMin){
-      windSpeed = 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
+        windSpeed = 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
     }
     else {
-      windSpeed = (sensorVoltage - voltageMin)*windSpeedMax/(voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
+        windSpeed = (sensorVoltage - voltageMin)*windSpeedMax/(voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
     }
 
-//Publish Results
+    //Publish Results
 
-     Serial.print(soil);
-     Serial.println("%");
-     Serial.print(soil2);
-     Serial.println("%");
+    if(loopCounter == 6*60*3){
+    
+    Serial.print(soil);
+    Serial.println("%");
+    Serial.print(soil2);
+    Serial.println("%");
     Particle.publish("SoilLog", (String)result + "%");
-     Serial.print("Voltage: ");
-     Serial.print(sensorVoltage);
-     Serial.print("\t"); 
-     Serial.print("Wind speed MPH: ");
-     Serial.println(windSpeed);
-    Particle.publish("WindSpeed", (String)windSpeed)
+    loopCounter = 0;
+    }
+    
+    Serial.print("Voltage: ");
+    Serial.print(sensorVoltage);
+    Serial.print("Wind speed MPH: ");
+    Serial.println(windSpeed);
+    Particle.publish("WindSpeed", (String)windSpeed);
 
-  delay(10800000);        // delay reads for every 3 hours
-
-
- 
-
+    delay(10000);        // delay reads for every 3 hours
 }
